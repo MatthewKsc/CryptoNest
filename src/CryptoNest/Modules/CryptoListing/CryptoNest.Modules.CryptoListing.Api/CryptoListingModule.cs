5cf64eabc0f975e1d2a901e31f1e51cfb2aeb685
MatthewKsc <55882615+MatthewKsc@users.noreply.github.com>
2023-06-03 19:55:08 +0200

    Add IModule and CryptoListingModule

---
+++ src/CryptoNest/Modules/CryptoListing/CryptoNest.Modules.CryptoListing.Api/CryptoListingModule.cs
@@ -0,0 +1,21 @@
+﻿using CryptoNest.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CryptoNest.Modules.CryptoListing.Api;
+
+internal class CryptoListingModule : IModule
+{
+    public const string BasePath = "crypto-listing-module";
+
+    public string Name { get; } = "CryptoListing";
+    public string Path => BasePath;
+    
+    public void Register(IServiceCollection services)
+    {
+    }
+
+    public void Use(IApplicationBuilder applicationBuilder)
+    {
+    }
+}
