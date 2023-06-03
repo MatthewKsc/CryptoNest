abbeef274b066bb90643c836b9f1e2569beba3e8
MatthewKsc <55882615+MatthewKsc@users.noreply.github.com>
2023-06-03 19:54:30 +0200

    Typo fix in Bootstrapper project

---
+++ src/CryptoNest/Bootstrapper/CryptoNest.Bootstrapper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using CryptoNest.Bootstrapper;
+using CryptoNest.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
+
+List<Assembly> assemblies = ModuleLoader.LoadAssemblies(builder.Configuration);
+List<IModule> modules = ModuleLoader.LoadModules(assemblies);
+
+foreach (IModule module in modules)
+{
+    module.Register(builder.Services);
+}
+
+foreach (IModule module in modules)
+{
+    module.Use(app);
+}
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
