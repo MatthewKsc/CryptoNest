3cc0cbdd75ab66ef1f0b1b6356ef779411cf42e1
MatthewKsc <55882615+MatthewKsc@users.noreply.github.com>
2023-06-04 13:40:40 +0200

    Add Configuration Handling for Modules

---
+++ src/CryptoNest/Shared/CryptoNest.Shared.Infrastructure/Modules/ModulesExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace CryptoNest.Shared.Infrastructure.Modules;
+
+public static class ModulesExtensions
+{
+    internal static WebApplicationBuilder ConfigureModules(this WebApplicationBuilder builder)
+    {
+        foreach (string setting in GetSettings("*", builder))
+        {
+            builder.Configuration.AddJsonFile(setting);
+        }
+        
+        foreach (string setting in GetSettings($"*.{builder.Environment.EnvironmentName}", builder))
+        {
+            builder.Configuration.AddJsonFile(setting);
+        }
+
+        return builder;
+    }
+    
+    private static IEnumerable<string> GetSettings(string pattern, WebApplicationBuilder  builder) =>
+        Directory.EnumerateFiles(builder.Environment.ContentRootPath, $"module.{pattern}.json", SearchOption.AllDirectories);
+}
