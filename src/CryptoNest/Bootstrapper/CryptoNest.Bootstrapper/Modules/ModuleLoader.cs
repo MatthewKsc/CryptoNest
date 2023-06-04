9807c37ac4eb5373188a6173e4eae272fbc970b1
MatthewKsc <55882615+MatthewKsc@users.noreply.github.com>
2023-06-04 13:41:39 +0200

    Refactor Loading of Modules and move it from Program.cs

--- src/CryptoNest/Bootstrapper/CryptoNest.Bootstrapper/Modules/ModuleLoader.cs
+++ src/CryptoNest/Bootstrapper/CryptoNest.Bootstrapper/Modules/ModuleLoader.cs
@@ -6,27 +6,52 @@ using System.Reflection;
 using CryptoNest.Shared.Abstractions.Modules;
 using Microsoft.Extensions.Configuration;
 
-namespace CryptoNest.Bootstrapper;
+namespace CryptoNest.Bootstrapper.Modules;
 
 internal static class ModuleLoader
 {
+    private const string ModuleNamespacePart = "CryptoNest.Modules.";
+    
     public static List<Assembly> LoadAssemblies(IConfiguration configuration)
     {
         List<Assembly> assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
         List<string> modulesFiles = GetModulesFiles(assemblies);
+
+        List<string> disabledModulesFiles = new();
+
+        foreach (string moduleFile in modulesFiles)
+        {
+            if (!moduleFile.Contains(ModuleNamespacePart))
+            {
+                continue;
+            }
+
+            string moduleName = moduleFile.Split(ModuleNamespacePart)[1]
+                .Split(".")[0]
+                .ToLowerInvariant();
+
+            bool isModuleEnabled = configuration.GetValue<bool>($"{moduleName}:module:enabled");
+
+            if (!isModuleEnabled)
+            {
+                disabledModulesFiles.Add(moduleFile);
+            }
+        }
+        
+        disabledModulesFiles.ForEach(disabledModule => modulesFiles.Remove(disabledModule));
         
         modulesFiles.ForEach(x => assemblies.Add(AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(x))));
 
         return assemblies;
     }
 
-    public static List<IModule> LoadModules(IEnumerable<Assembly> assemblies) =>
+    public static IReadOnlyCollection<IModule> LoadModules(IEnumerable<Assembly> assemblies) =>
         assemblies.SelectMany(x => x.GetTypes())
             .Where(x => typeof(IModule).IsAssignableFrom(x) && !x.IsInterface)
             .OrderBy(x => x.Name)
             .Select(Activator.CreateInstance)
             .Cast<IModule>()
-            .ToList();
+            .ToArray();
 
     private static List<string> GetModulesFiles(List<Assembly> assemblies)
     {
--- src/CryptoNest/Bootstrapper/CryptoNest.Bootstrapper/Program.cs
+++ src/CryptoNest/Bootstrapper/CryptoNest.Bootstrapper/Program.cs
@@ -1,25 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
-using CryptoNest.Bootstrapper;
-using CryptoNest.Shared.Abstractions.Modules;
+﻿using CryptoNest.Bootstrapper.Modules;
+using CryptoNest.Shared.Infrastructure.Modules;
 using Microsoft.AspNetCore.Builder;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args)
+    .ConfigureModules();
+
+builder.RegisterModules();
+
 WebApplication app = builder.Build();
 
-List<Assembly> assemblies = ModuleLoader.LoadAssemblies(builder.Configuration);
-List<IModule> modules = ModuleLoader.LoadModules(assemblies);
-
-foreach (IModule module in modules)
-{
-    module.Register(builder.Services);
-}
-
-foreach (IModule module in modules)
-{
-    module.Use(app);
-}
-
+app.UseModules(app.Logger);
 
 app.MapGet("/", () => "Hello World!");
 
