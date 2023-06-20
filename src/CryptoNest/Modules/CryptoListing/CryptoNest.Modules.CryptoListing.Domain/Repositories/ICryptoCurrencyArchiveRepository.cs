fea1eff93d97e386da9af67bed3806b6cb86262a
MatthewKsc <55882615+MatthewKsc@users.noreply.github.com>
2023-06-20 22:50:01 +0200

    CN-007 Add Crypto Listing Domain

---
+++ src/CryptoNest/Modules/CryptoListing/CryptoNest.Modules.CryptoListing.Domain/Repositories/ICryptoCurrencyArchiveRepository.cs
@@ -0,0 +1,12 @@
+﻿using CryptoNest.Modules.CryptoListing.Domain.Entities;
+
+namespace CryptoNest.Modules.CryptoListing.Domain.Repositories;
+
+public interface ICryptoCurrencyArchiveRepository
+{
+    Task<CryptoCurrencyArchive> GetByIdAsync(int id);
+    Task<CryptoCurrencyArchive> GetBySymbolAsync(string symbol);
+    Task<IEnumerable<CryptoCurrencyArchive>> GetAllAsync();
+    Task AddAsync(CryptoCurrencyArchive currency);
+    Task AddRangeAsync(IEnumerable<CryptoCurrencyArchive> currencies);
+}
