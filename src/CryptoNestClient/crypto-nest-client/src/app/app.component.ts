7965138212b30b7c97b47b593c3bc4a22bea07ce
MatthewKsc <55882615+MatthewKsc@users.noreply.github.com>
2023-07-07 17:44:35 +0200

    CN-014 Update app component with routes  and new modules

--- src/CryptoNestClient/crypto-nest-client/src/app/app.component.ts
+++ src/CryptoNestClient/crypto-nest-client/src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, computed, signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title: WritableSignal<string> = signal<string>('crypto-nest-client');
-}
+export class AppComponent { }
